843cc6d331d8ad3e35910b162950d7375f72fa27
clarakohrman <claragudrun@yahoo.com>
2020-09-03 21:07:00 -0400

    added equations to script

--- activity 1/activity1_script.r
+++ activity 1/activity1_script.r
@@ -1,1 +1,17 @@
 print("hello R world")
+#52 times 37
+52*37
+#calculation 2
+22-(82/4)
+#name my number
+a<-45
+#calculation 3
+a-4
+#make vector
+b<- c(10,7,18)
+#multiply vector
+b*2
+#vector 2
+c<- c(41,1000,89,2)
+#divide vector
+c/2
